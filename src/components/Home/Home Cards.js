da4fd66e9e68bb0f199c5d3e321fc7afa5d9ab15
Walter Valentini <walval@outlook.it>
2023-05-17 11:55:32 +0200

    fixed responsive problem on homeCards

--- src/components/Home/Home Cards.js
+++ src/components/Home/Home Cards.js
@@ -8,9 +8,10 @@ import './Home.scss';
 
 const HomeCards = () => {
   const [products, setProducts] = useState([]);
-  const base64prefix = "data:image/jpeg;base64,"
+  const base64prefix = 'data:image/jpeg;base64,';
   useEffect(() => {
-    axios.get(URL_PRODUCTS)
+    axios
+      .get(URL_PRODUCTS)
       .then(response => {
         setProducts(response.data);
       })
@@ -21,38 +22,39 @@ const HomeCards = () => {
 
   return (
     <div className="products-section">
-    <h3>Our Products</h3>
-    <Slider
-      slidesToShow={3} // Numero di prodotti da mostrare contemporaneamente
-      slidesToScroll={1} // Numero di prodotti da scorrere alla volta
-      infinite={true} // Abilita lo scorrimento infinito
-      responsive={[
-        {
-          breakpoint: 768, // Modificare la dimensione del breakpoint se necessario
-          settings: {
-            slidesToShow: 1,
+      <h3>Our Products</h3>
+      <Slider
+        slidesToShow={4}
+        swipeToSlide={true}
+        swipeThreshold={50}
+        infinite={true}
+        responsive={[
+          {
+            breakpoint: 351,
+            settings: {
+              slidesToShow: 1,
+            },
           },
-        },
-      ]}
-    >
-      {products.map(product => (
-        <div key={product.id} className="card">
-          <img src={base64prefix + product.cover} alt={product.name} className="card-image" />
-          <div className="card-body">
-            <h5 className="card-title">{product.name}</h5>
-            <p className="card-text">
-              Is Vegan: {product.vegan ? 'Yes' : 'No'}
-              <br />
-              Is Nut-free: {product.nutfree ? 'Yes' : 'No'}
-              <br />
-              Is Dairy-free: {product.dairyFree ? 'Yes' : 'No'}
-            </p>
+        ]}
+      >
+        {products.map(product => (
+          <div key={product.id} className="card">
+            <img src={base64prefix + product.cover} alt={product.name} className="card-image" />
+            <div className="card-body">
+              <h5 className={`card-title ${window.innerWidth < 500 ? 'small' : ''}`}>{product.name}</h5>
+              <p className={`card-text ${window.innerWidth < 500 ? 'small' : ''}`}>
+                Vegan: {product.vegan ? 'Yes' : 'No'}
+                <br />
+                Nut-free: {product.nutfree ? 'Yes' : 'No'}
+                <br />
+                Dairy-free: {product.dairyFree ? 'Yes' : 'No'}
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </Slider>
-  </div>
-);
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default HomeCards;
